7b57aac513ebbb90d9e79d02c903e85b5d3af626
le.van.hai <le.van.hai@miyatsu.vn>
2018-05-28 13:53:36 +0700

    update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,14 +2,20 @@
 @section('title') 404 :: @parent @stop
 
 @section('content')
-    {!! Form::open(['url' => 'login', 'class' => 'form-signin', 'role' => 'form']) !!}
+    {!! Form::open(['url' => 'logout', 'class' => 'form-signin', 'id' => 'form-signin', 'role' => 'form']) !!}
     <div class=" card-box">
         <div class="panel-heading">
             <h3 class="text-center text-uppercase">{{{ trans('message.404_msg') }}}<br></h3>
         </div>
         <div class="panel-body page-not-found">
             @if(Auth::check())
-                <a href="{{ url('/') }}">{{{ trans('button.back_to_home') }}}</a>
+                @if(Auth::user()->authority == config('constants.authority.client'))
+                    <a href="http://weechat.local.vn/logout" onclick="event.preventDefault(); document.getElementById('form-signin').submit();">
+                        {{trans('menu.logout')}}
+                    </a>
+                @else
+                    <a href="{{ url('/') }}">{{{ trans('button.back_to_home') }}}</a>
+                @endif
             @else
                 <a href="{{ url('login') }}" >{{{ trans('button.back_to_home') }}}</a>
             @endif
